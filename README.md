a275d4d69c59a912afb5b0000f89eb2132863d51
Alexander Eckert <eckertalex@protonmail.com>
2021-12-02 09:37:15 +0100

    feat: add solution for day 2

--- README.md
+++ README.md
@@ -12,7 +12,7 @@
 <!--SOLUTIONS-->
 
 [![Day](https://badgen.net/badge/01/%E2%98%85%E2%98%85/green)](src/day01)
-![Day](https://badgen.net/badge/02/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/02/%E2%98%85%E2%98%85/green)](src/day02)
 ![Day](https://badgen.net/badge/03/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/04/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/05/%E2%98%86%E2%98%86/gray)
@@ -69,16 +69,16 @@ yarn start 1
 
 ```
 Day 01
-Time part 1: 0.77ms
-Time part 2: 1.09ms
-Both parts: 1.86ms
+Time part 1: 0.38ms
+Time part 2: 0.41ms
+Both parts: 0.79ms
 ```
 
 ```
 Day 02
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 0.77ms
+Time part 2: 0.7ms
+Both parts: 1.47ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 2/50
-Total time: 1.86ms
+Total stars: 4/50
+Total time: 2.26ms
 ```
 
 <!--/RESULTS-->
---
+++ src/day02/index.ts
@@ -0,0 +1,65 @@
+import run from 'aocrunner'
+
+function parseInput(rawInput: string) {
+  return rawInput
+}
+
+function part1(rawInput: string) {
+  let input = parseInput(rawInput).split('\n')
+
+  let horizontalPosition = 0
+  let depth = 0
+  for (let command of input) {
+    let [direction, amount] = command.split(' ')
+    if (direction === 'forward') {
+      horizontalPosition += Number(amount)
+    }
+    if (direction === 'down') {
+      depth += Number(amount)
+    }
+    if (direction === 'up') {
+      depth -= Number(amount)
+    }
+  }
+
+  return horizontalPosition * depth
+}
+
+function part2(rawInput: string) {
+  let input = parseInput(rawInput).split('\n')
+
+  let horizontalPosition = 0
+  let depth = 0
+  let aim = 0
+  for (let command of input) {
+    let [direction, amount] = command.split(' ')
+    if (direction === 'forward') {
+      horizontalPosition += Number(amount)
+      depth += aim * Number(amount)
+    }
+    if (direction === 'down') {
+      aim += Number(amount)
+    }
+    if (direction === 'up') {
+      aim -= Number(amount)
+    }
+  }
+
+  return horizontalPosition * depth
+}
+
+run({
+  part1: {
+    tests: [
+      // { input: ``, expected: "" },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      // { input: ``, expected: "" },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+})
